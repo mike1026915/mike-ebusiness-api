25119cbf364b0d3eb25f7f5334747c3343b21df1
mike_fang <mike1026915.cs96@g2.nctu.edu.tw>
2019-11-01 22:03:22 +0800

    update DB URI

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
     pass
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgres://aehzefdybddfcl:2ce386813249e02b9049ed23201cf2a6a5c5f9710b8f34cb73d74240fe9ca2fc@ec2-107-21-201-238.compute-1.amazonaws.com:5432/ddmsht4bl80g3n'
+    SQLALCHEMY_DATABASE_URI = 'postgres://ebuijlzvchiehu:c4613c281271dda2f7b1deec47dfe1506d4c87e23d905916b614aaac1f23ea92@ec2-174-129-252-228.compute-1.amazonaws.com:5432/d39479odedb5t7'
  #'postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(db)s' % POSTGRES
     DEBUG = False
     ENV = "production"
